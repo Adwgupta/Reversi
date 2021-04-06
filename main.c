6f32ec53e6e37c4a8905733b4b40dfc5615e5634
Adityaa Gupta <adityaa002g@gmail.com>
2021-04-06 23:19:58 +0100

    Created a working function for game results

--- main.c
+++ main.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
+#define file "othello-results.txt"
+
+FILE *results;
+
+void resultsFile();
 
 void main(){
    printf("Hello There!\nThis is the creation of the main file and the first commit of this project");
+   resultsFile();
+}
+//Function to print out game results into a txt file
+void resultsFile(){
+   if ((results = fopen(file, "w")) == NULL) { 
+      //display error if file cannot be opened
+      perror;
+   }
+   else {
+      fprintf(results, "Trial run");
+   }
 }
