a58ea24373a407db756d3e2d0fc4b6be53d454c0
Adityaa Gupta <adityaa002g@gmail.com>
2021-04-07 01:56:36 +0100

    Added a comment with link to this repo

--- Code/main.c
+++ Code/main.c
@@ -1,3 +1,8 @@
+/**  
+ *   Link to Github repo
+ *   https://github.com/Adwgupta/Reversi
+ */
+
 #include <stdio.h>
 //Name for the results txt file
 #define file "othello-results.txt"
@@ -19,7 +24,7 @@ void resultsFile(char printingMessage[]){
 //Main function to initialise board & players, place tokens and play game
 void main(){
    //trial message to be printed out
-   char message[] = "Hello There!\nGeneral Kenobi!\tYou *are* a bold one!";
+   char message[] = "Hello There!\nGeneral Kenobi!\tYou ARE a bold one!";
    //Printing message to the console
    printf("%s", message);
    //prints out the game results to the pre-defined txt file
