5fdc85cc7ad05711a13b3adab346e510ca3f843d
Adityaa Gupta <adityaa002g@gmail.com>
2021-04-07 10:30:29 +0100

    Added a feature to print out the time at which the program executes

--- Code/main.c
+++ Code/main.c
@@ -4,25 +4,34 @@
  */
 
 #include <stdio.h>
+#include <time.h>
 //Name for the results txt file
 #define file "othello-results.txt"
 //File pointer
 FILE *fp;
 
-//Function to print out print game results out to the pre-defined txt file
-void resultsFile(char printingMessage[]) {
-   //Creates the desired output txt file, replaces the previous file contents with the new code output
-   fp = fopen(file, "a");
-   //For now we're printing out message from main
-   fprintf(fp, printingMessage);
+/**
+ * Function to print out print game results out to the pre-defined txt file
+ * the function also takes in a file pointer, this will allow a message to be printed out to the console
+ * as well as the txt file
+ */
+void resultsFile(FILE *out, char printingMessage[]) {
+   //Variable t that will store the current time
+   time_t t;
+   //Seeds variable t with current time
+   time(&t);
+   //For now we're printing out the message string/array from main and the current time
+   fprintf(out, "Message:\n%s\nExecuted at:\n%s", printingMessage, ctime(&t));
 }
 
 //Main function to initialise board & players, place tokens and play game
 void main() {
    //trial message to be printed out
    char message[] = "Kenobi:\t\t\"Hello There!\"\nGrevious:\t\"General Kenobi!\"\nGrevious:\t\"You ARE a bold one!\"\n";
-   //Printing message to the console
-   printf("%s", message);
-   //prints out the game results to the pre-defined txt file
-   resultsFile(message);
+   //Creates the desired output txt file, replaces the previous file contents with the new code output
+   fp = fopen(file, "a");
+   //prints out the message to the console
+   resultsFile(stdout, message);
+   //prints out the message to the txt file
+   resultsFile(fp, message);
 }
