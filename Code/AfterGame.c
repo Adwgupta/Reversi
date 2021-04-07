c3ca447b686611b3a9e01a3271e8763f92f90404
Adityaa Gupta <adityaa002g@gmail.com>
2021-04-07 11:04:26 +0100

    Made some changes to file opening

--- Code/AfterGame.c
+++ Code/AfterGame.c
@@ -3,13 +3,17 @@
 #include "AfterGame.h"
 
 //Prints out game results
-void resultsFile(FILE *out, char printingMessage[]) {
-    //Creates the desired output txt file, replaces the previous file contents with the new code output
-    fp = fopen(file, "a");
+void resultsFile(FILE *out) {
+    /**
+     * Creates the desired output txt file and appends the results to the file
+     * if the file doesnt exist, it will be created
+     */
+    if ((fp = fopen(file, "a")) == NULL)
+		perror;
     //Variable t that will store the current time
     time_t t;
     //Seeds variable t with current time
     time(&t);
     //For now we're printing out the message string/array from main and the current time
-    fprintf(out, "Message:\n%s\nExecuted at:\n%s", printingMessage, ctime(&t));
+    fprintf(out, "Executed at:\n%s", ctime(&t));
 }
